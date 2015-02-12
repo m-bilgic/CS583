a2592efc09972159137c7d05bd41f4a181b7ca05
jshao3 <jshao3@hawk.iit.edu>
2015-02-12 17:43:18 -0600

    fix some problems in the marginally_independent implement the minial I-Map also with some function used in it.

--- ch2/probs.py
+++ ch2/probs.py
@@ -13,6 +13,12 @@ class Probs(object):
         s = [2 for _ in range(n)]
         self.prob_table = np.zeros(tuple(s))
         self.prob_table[:] = 1./np.power(2, n)
+        self.name=[]
+        nameList=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        if name is not None:
+            self.name=name #variables name
+        else:
+            self.name=nameList[:n]
     
     def read_prob_table_from_csv_file(self, file_name):
         with open(file_name, 'rb') as csv_file:
@@ -26,8 +32,8 @@ class Probs(object):
         n_old = len(self.prob_table.shape)
         n_new = len(variables)
         combs = itertools.product(range(2), repeat=n_new)
-                
-        p_new = Probs(n_new)
+
+        p_new = Probs(n_new,self.name[variables[0]:(variables[-1]+1)])
         
         for comb in combs:
             index = []
@@ -41,29 +47,32 @@ class Probs(object):
     
     def conditional(self, vars1, vars2):
         '''Prob(vars1|vars2)'''
-        joint_vars = list(vars1)
-        joint_vars.extend(vars2)
-        numerator = self.marginal(joint_vars)
-        denominator = self.marginal(vars2) # this could be done more efficiently by marginalizing over numerator
-        #denominator = numerator.marginal(range(len(joint_vars))[len(vars1):]) # this could be done more efficiently by marginalizing over numerator        
-        combs = itertools.product(range(2), repeat=len(vars2))
-        for comb in combs:
-            index = []
-            for _ in range(len(vars1)):
-                index.append(slice(None))
-                for c in comb:
-                    index.append(c)
-            numerator.prob_table[tuple(index)] /= denominator.prob_table[comb]
-        return numerator
+        if len(vars2)==0:
+            return self.marginal(vars1)
+        else:
+            joint_vars = list(vars1)
+            joint_vars.extend(vars2)
+            numerator = self.marginal(joint_vars)
+            denominator = self.marginal(vars2) # this could be done more efficiently by marginalizing over numerator
+            #denominator = numerator.marginal(range(len(joint_vars))[len(vars1):]) # this could be done more efficiently by marginalizing over numerator
+            combs = itertools.product(range(2), repeat=len(vars2))
+            for comb in combs:
+                index = []
+                for _ in range(len(vars1)):
+                    index.append(slice(None))
+                    for c in comb:
+                        index.append(c)
+                numerator.prob_table[tuple(index)] /= denominator.prob_table[comb]
+            return numerator
             
 
 def marginally_independent(jp, var1, var2):
     '''Given a joint distribution jp, test if var1 is marginally independent of var2
        Return True if independent; False otherwise.
     '''
-    union_table = jp.marginal([var1,var2]).prob_table
-    var1_table = jp.marginal([var1]).prob_table
-    var2_table = jp.marginal([var2]).prob_table
+    union_table = jp.marginal((var1+var2)).prob_table
+    var1_table = jp.marginal(var1).prob_table
+    var2_table = jp.marginal(var2).prob_table
     for i in range(len(var1_table)):
         for j in range(len(var2_table)):
             u=union_table[tuple([i,j])]
@@ -76,30 +85,44 @@ def marginally_independent(jp, var1, var2):
 def conditionally_independent(jp, var1, var2, var3):
     '''Given a joint distribution jp, test if var1 is marginally independent of var2 given var3
        Return True if independent; False otherwise.
+       P(V1|V2,V3)=P(V1|V3) <-->V1 is independent of V2 given V3
     '''
-    var2.extend(var3)
-    three_var_table = jp.conditional(var1,var2).prob_table #P(V1|V2,V3)
+    assert len(var2)==1
+    totLen=len(var1)+len(var2)+len(var3)
+    var23=var2+var3
+    total_var_table = jp.conditional(var1,var23).prob_table #P(V1|V2,V3)
     var_table=jp.conditional(var1,var3).prob_table #P(V1|V3)
-    for i in range(var_table.shape[0]): #var1
-        for k in range(2):#binary var2
-            for j in range(var_table.shape[1]): #var3
-                if math.fabs(three_var_table[i,k,j]-var_table[i,j])>1e-10:
-                    return False
+    combs=itertools.product(range(2),repeat=totLen)
+    for comb in combs:
+        c=list(comb)
+        c.pop(1)#var2 is not included; 0000->0?00 0001->0?01; ? could be 0 or 1
+        if math.fabs(total_var_table[tuple(comb)]-var_table[tuple(c)])>1e-10:
+            return False
+    return True
+
+def print_conditionally_independent(jp, var1, var2, var3):
+    var3_name=""
+    for v in var3:
+        var3_name+= str(jp.name[v])
+        var3_name+=" "
+    if not conditionally_independent(jp,var1,var2,var3):
+        print(str(jp.name[var1[0]])+" is not independent with "+ str(jp.name[var2[0]])+" given "+var3_name)
+        return False
+    print(str(jp.name[var1[0]])+" is independent with "+ str(jp.name[var2[0]])+" given "+var3_name)
     return True
 
 
-        
 if __name__ == '__main__':
     
     file_name = 'hw1.csv'
     jp = Probs(n=4)
     jp.read_prob_table_from_csv_file(file_name)
-    
+
     print "MARGINALS\n"
-    
-    for i in range(4):    
+
+    for i in range(4):
         print "P(%d)\n%s\n" %(i, jp.marginal([i]).prob_table)
-     
+
     print "\nCONDITIONALS\n"
     for i in range(4):
         for j in range(i+1, 4):
--- ch3/structure_search.py
+++ ch3/structure_search.py
@@ -1,4 +1,46 @@
 from ch2.probs import Probs, marginally_independent, conditionally_independent
+import itertools
+
+def print_conditionally_independent_multi_vars(jp,var1,var2,var3):
+    var2_name=""
+    for v in var2:
+        var2_name+= str(jp.name[v])
+        var2_name+=" "
+    var3_name=""
+    for v in var3:
+        var3_name+= str(jp.name[v])
+        var3_name+=" "
+    if not conditionally_independent_multi_vars(jp,var1,var2,var3):
+        print(str(jp.name[var1[0]])+" is not independent with "+ var2_name+"given "+var3_name)
+        return False
+    print(str(jp.name[var1[0]])+" is independent with "+ var2_name+"given "+var3_name)
+    return True
+
+def conditionally_independent_multi_vars(jp,var1,var2,var3):
+    for v2 in var2:
+        if not conditionally_independent(jp,var1,[v2],var3):
+            return False
+    return True
+
+def marginally_independent_multi_vars(jp,var1,var2):
+    for v2 in var2:
+        if not marginally_independent(jp,var1,[v2]):
+            return False
+    return True
+
+def get_subset(given_Set):
+    all_subset=[]
+    for i in range(len(given_Set)+1):
+        combs=itertools.combinations(given_Set,i)
+        for c in combs:
+            all_subset.append(list(c))
+    return all_subset
+
+def subtract_set(full_set,sub_set):
+    assert len(full_set)>=len(sub_set)
+    for v in sub_set:
+        full_set.remove(v)
+    return full_set
 
 def minimal_imap(jp, variable_order):
     '''Given a joint distribution and a variable order,
@@ -6,7 +48,23 @@ def minimal_imap(jp, variable_order):
        Algorithm 3.2 in the PGM book.
        http://pgm.stanford.edu/Algs/page-79.pdf
     '''
-    raise NotImplementedError('Not implemented yet.')
+    post_set=[]
+    for var_order in variable_order:
+        if len(post_set)==0:
+            post_set.append(var_order)
+        else:
+            u_set=get_subset(post_set)
+
+            for u in u_set:
+                # var2=[]
+                copy_set=list(post_set)
+                var2=subtract_set(copy_set,u)
+                if conditionally_independent_multi_vars(jp,[var_order],var2,u):
+                    for v in u:
+                        print str(jp.name[v])+"->"+str(jp.name[var_order])
+                    break
+            post_set.append(var_order)
+
 
 def iequivalent_structures(jp):
     '''Given a joint distribution find all i-equilavent structures that
@@ -19,4 +77,7 @@ def iequivalent_structures(jp):
     raise NotImplementedError('Not implemented yet.')
 
 if __name__ == '__main__':
-    pass
+    file_name = 'hw1.csv'
+    jp = Probs(n=4)
+    jp.read_prob_table_from_csv_file(file_name)
+    minimal_imap(jp, [3,2,1,0])
