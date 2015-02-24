5046904810fcf809251a33dd6f6eeea5adb7f94f
Mustafa Bilgic <mbilgic@iit.edu>
2015-02-24 10:00:09 -0600

    The Graph structure and data utils.

---
+++ project/data_utils.py
@@ -0,0 +1,11 @@
+def load_cora(data_path):
+    '''
+    Create a DirectedGraph object and add Nodes and Edges
+    '''
+    pass
+
+def load_citeseer(data_path):
+    '''
+    Create a DirectedGraph object and add Nodes and Edges
+    '''
+    pass
