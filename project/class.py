c9c49f3fda65c9bb9025e624f433f163d6cee4e8
jshao3 <jshao3@hawk.iit.edu>
2015-02-23 22:49:26 -0600

    Graph is a base class Direct_Graph is a subclass of Graph Add get_incomes() and get_outcomes() function

--- project/class.py
+++ project/class.py
@@ -1,5 +1,10 @@
 __author__ = 'wolfshjj'
 
+def abstract():
+    import inspect
+    caller = inspect.getouterframes(inspect.currentframe())[1][3]
+    raise NotImplementedError(caller + ' must be implemented in subclass')
+
 class Node(object):
     def __init__(self,iD,features,label):
         self.id=iD
@@ -8,17 +13,21 @@ class Node(object):
 
 
 class Edge(object):
-    def __init__(self,cited,citing):#A, B: A->B
-        self.edge=(cited,citing)
+    def __init__(self,A,B):#A, B: A->B
+        self.edge=(A,B)
 
 class Graph(object):
-    def __init__(self,content_path,cites_path):
-        self.nodes_list=[]
-        self.edges_list=[]
-        self.read_nodes(content_path)
-        self.read_edges(cites_path)
+    def __init__(self,content_path):
+        self.nodes_dict=self.get_nodes(content_path)
 
-    def read_nodes(self,path):
+    # def add_node(self,n):
+    #     abstract()
+    #
+    # def add_edge(self,e):
+    #     abstract()
+
+    def get_nodes(self,path):
+        nodes_dict={}
         if path.split('.')[-1]!='content':
             print 'wrong content file'
         else:
@@ -28,10 +37,52 @@ class Graph(object):
                 if not line:
                     break
                 line_info=line.split('\n')[0].split('\t')
-                self.nodes_list.append(Node(line_info[0],line_info[1:-1],line_info[-1]))
+                nodes_dict[line_info[0]]=Node(line_info[0],line_info[1:-1],line_info[-1])
+                # self.nodes_list.append(Node(line_info[0],line_info[1:-1],line_info[-1]))
             node_file.close()
+        return nodes_dict
 
-    def read_edges(self,path):
+    def get_edges(self,path):
+        abstract()
+        # if path.split('.')[-1]!='cites':
+        #     print 'wrong cites file'
+        # else:
+        #     edge_file=open(path,'r')
+        #     while True:
+        #         line=edge_file.readline()
+        #         if not line:
+        #             break
+        #         line_info=line.split('\n')[0].split('\t')
+        #
+        #         self.edges_list.append(Edge(line_info[0],line_info[1]))
+        #     edge_file.close()
+
+class Direct_Graph(Graph):
+    def __init__(self,content_path,cites_path):
+        super(Direct_Graph,self).__init__(content_path)
+        # self.nodes_dict=super(Direct_Graph,self).get_nodes(content_path)
+        self.edges_list=self.get_edges(cites_path)
+        self.outcomes=self.get_outcomes()
+        self.incomes=self.get_incomes()
+
+    # def get_nodes(self,path):
+    #     nodes_dict={}
+    #     if path.split('.')[-1]!='content':
+    #         print 'wrong content file'
+    #     else:
+    #         node_file=open(path,'r')
+    #         while True:
+    #             line=node_file.readline()
+    #             if not line:
+    #                 break
+    #             line_info=line.split('\n')[0].split('\t')
+    #             nodes_dict[line_info[0]]=Node(line_info[0],line_info[1:-1],line_info[-1])
+    #             # self.nodes_list.append(Node(line_info[0],line_info[1:-1],line_info[-1]))
+    #         node_file.close()
+    #     return nodes_dict
+
+    def get_edges(self,path):
+        edges_list=[]
         if path.split('.')[-1]!='cites':
             print 'wrong cites file'
         else:
@@ -42,11 +93,34 @@ class Graph(object):
                     break
                 line_info=line.split('\n')[0].split('\t')
 
-                self.edges_list.append(Edge(line_info[0],line_info[1]))
+                edges_list.append(Edge(line_info[0],line_info[1]))
             edge_file.close()
+        return edges_list
+
+    def get_outcomes(self):
+        outcomes={}
+        for edge in self.edges_list:
+            if edge.edge[0] not in outcomes.keys():
+                outcomes[edge.edge[0]]=list()
+                outcomes[edge.edge[0]].append(self.nodes_dict[edge.edge[1]])
+            else:
+                outcomes[edge.edge[0]].append(self.nodes_dict[edge.edge[1]])
+        return outcomes
+
+    def get_incomes(self):
+        incomes={}
+        for edge in self.edges_list:
+            if edge.edge[1] not in incomes.keys():
+                incomes[edge.edge[1]]=list()
+                incomes[edge.edge[1]].append(self.nodes_dict[edge.edge[0]])
+            else:
+                incomes[edge.edge[1]].append(self.nodes_dict[edge.edge[0]])
+        return incomes
+
 
 content_path="G:/IIT/CS583 TA/project/cora/cora.content"
 cites_path="G:/IIT/CS583 TA/project/cora/cora.cites"
-a=Graph(content_path,cites_path)
+a=Direct_Graph(content_path,cites_path)
+
 # a.read_nodes("G:/IIT/CS583 TA/project/cora/cora.content")
-print(a.nodes_list[-1].id)
+print(a.outcomes)
