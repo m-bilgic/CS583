996e223c289dc9fa09c6412b08a827cc6affb36e
Mustafa Bilgic <mbilgic@iit.edu>
2015-03-01 13:51:10 -0600

    Replicated classifiers and data_utils files.

---
+++ project/solutions/classifiers.py
@@ -0,0 +1,121 @@
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.linear_model import LogisticRegression
+
+def get_class( kls ):
+    parts = kls.split('.')
+    module = ".".join(parts[:-1])
+    m = __import__( module )
+    for comp in parts[1:]:
+        m = getattr(m, comp)
+    return m
+
+def abstract():
+    import inspect
+    caller = inspect.getouterframes(inspect.currentframe())[1][3]
+    raise NotImplementedError(caller + ' must be implemented in subclass')
+
+class Aggregator(object):
+    
+    def __init__(self, directed = False):
+        self.directed = directed
+    
+    def aggregate(self, graph, node, train_indices): 
+        # TODO: We need to keep track of predicted labels somewhere or pass them to this function
+        # We can create a predicted label variable in the Node class and set it everytime we predict something for it
+        abstract()
+        # for tr in train_indices:
+        #     node=graph.node_list[tr]
+        #     if self.directed:
+        #         out_neighbors=graph.get_out
+
+
+class Classifier(object):
+    '''
+    The base classifier object
+    '''
+
+    def __init__(self, scikit_classifier_name):
+        self.scikit_classifier_name = scikit_classifier_name
+
+    
+    def fit(self, graph, train_indices):
+        '''
+        Create a scikit-learn classifier object and fit it using the Nodes of the Graph
+        that are referenced in the train_indices
+        '''
+        abstract()
+    
+    def predict(self, graph,observed_indices, test_indices):
+        '''
+        This function should be called only after the fit function is called.
+        Predict the labels of test Nodes assuming the labels of the train Nodes are observed.
+        '''
+        abstract()
+
+class LocalClassifier(Classifier):
+
+    def __init__(self,scikit_classifier_name):
+        super(LocalClassifier,self).__init__(scikit_classifier_name)
+        classifer_class=get_class(scikit_classifier_name)
+        self.classifier=classifer_class()
+
+    def fit(self, graph, train_indices):
+        '''
+        Create a scikit-learn classifier and fit it to the Node attributes
+        ''' 
+        # abstract()
+        X=[graph.node_list[t].feature_vector for t in train_indices]
+        y=[graph.node_list[t].label for t in train_indices]
+        self.classifier.fit(X,y)
+
+
+    def predict(self, graph,observed_indices, test_indices):
+        '''
+        This function should be called only after the fit function is called.
+        Predict the labels of test Nodes assuming the labels of the train Nodes are observed.
+        Use only the node attributes for prediction.
+        '''
+        # abstract()
+        X=[graph.node_list[t].feature_vector for t in test_indices]
+        return self.classifier.predict(X)
+
+class RelationalClassifier(Classifier):
+    
+    def __init__(self, scikit_classifier_name, aggregator, use_node_attributes = False):
+        super(RelationalClassifier, self).__init__(scikit_classifier_name)
+        self.aggregator = aggregator
+        self.use_node_attributes = use_node_attributes
+    
+    def fit(self, graph, train_indices):
+        '''
+        Create a scikit-learn classifier and fit it to relational (+ potentially node) attributes
+        ''' 
+        abstract()
+    
+    def predict(self, graph,observed_indices, test_indices):
+        '''
+        This function should be called only after the fit function is called.
+        Predict the labels of test Nodes assuming the labels of the train Nodes are observed.
+        Use relational (+ potentially node) attributes for prediction.
+        '''
+        abstract()
+
+class ICA(Classifier):
+    
+    def __init__(self, local_classifier, relational_classifier, max_iteration = 10):
+        self.local_classifier = local_classifier
+        self.relational_classifier = relational_classifier
+        self.max_iteration = 10
+    
+    
+    def fit(self, graph, train_indices):
+        self.local_classifier.fit(graph, train_indices)
+        self.relational_classifier.fit(graph, train_indices)
+    
+    def predict(self, graph, observed_indices, test_indices):
+        '''
+        This function should be called only after the fit function is called.
+        Iteratively, predict the labels of all Nodes that are in the graph but not in train indices.
+        '''
+        abstract()
+    
---
+++ project/solutions/data_utils.py
@@ -0,0 +1,112 @@
+from project.assigned.graph import DirectedGraph, Node, Edge
+import csv
+
+def _load_linqs_graph(data_path):
+    '''
+    Create a DirectedGraph object and add Nodes and Edges
+    '''
+    linqs_graph=DirectedGraph()
+    for path in data_path:
+        if path.split('.')[-1]=='content':  #.content file
+            # with open(path, 'rb') as csv_file:
+            #     csv_reader = csv.reader(csv_file)
+            #     for row in csv_reader:
+            #         linqs_graph.add_node(Node(row[0],row[1:-1],row[-1]))
+
+            node_file=open(path,'r')
+            while True:
+                line=node_file.readline()    #read line
+                if not line:
+                    break
+                line_info=line.split('\n')[0].split('\t')
+                cl=linqs_graph.map_class_str_to_int(line_info[-1])
+                linqs_graph.add_node(Node(float(line_info[0]),map(float,line_info[1:-1]),cl))# id, feature vector, label
+            node_file.close()
+
+        elif path.split('.')[-1]=='cites':#.cites file
+            edge_file=open(path,'r')
+            while True:
+                line=edge_file.readline()   #read line
+                if not line:
+                    break
+                line_info=line.split('\n')[0].split('\t')
+                linqs_graph.add_edge(Edge(line_info[0],line_info[1]))# cited -> citing
+                                                                    # the more out neighbors the more important
+            edge_file.close()
+    return linqs_graph
+
+def load_cora(data_path):
+    '''
+    Load cora data set
+    '''
+    return _load_linqs_graph(data_path)
+
+def load_citeseer(data_path):
+    '''
+    Load citeseer data set
+    '''
+    return _load_linqs_graph(data_path)
+
+# def load_cora(data_path):
+#     '''
+#     Create a DirectedGraph object and add Nodes and Edges
+#     '''
+#     cora=DirectedGraph()
+#     for path in data_path:
+#         if path.split('.')[-1]=='content':  #.content file
+#             node_file=open(path,'r')
+#             while True:
+#                 line=node_file.readline()    #read line
+#                 if not line:
+#                     break
+#                 line_info=line.split('\n')[0].split('\t')
+#                 cora.add_node(Node(line_info[0],line_info[1:-1],line_info[-1]))# id, feature vector, label
+#             node_file.close()
+#
+#         elif path.split('.')[-1]=='cites':#.cites file
+#             edge_file=open(path,'r')
+#             while True:
+#                 line=edge_file.readline()   #read line
+#                 if not line:
+#                     break
+#                 line_info=line.split('\n')[0].split('\t')
+#                 cora.add_edge(Edge(line_info[0],line_info[1]))# cited -> citing
+#             edge_file.close()
+#     return cora
+
+
+# def load_citeseer(data_path):
+#     '''
+#     Create a DirectedGraph object and add Nodes and Edges
+#     '''
+#     citesser=DirectedGraph()
+#     for path in data_path:
+#         if path.split('.')[-1]=='content':
+#             node_file=open(path,'r')
+#             while True:
+#                 line=node_file.readline()
+#                 if not line:
+#                     break
+#                 line_info=line.split('\n')[0].split('\t')
+#                 citesser.add_node(Node(line_info[0],line_info[1:-1],line_info[-1]))
+#             node_file.close()
+#
+#         elif path.split('.')[-1]=='cites':
+#             edge_file=open(path,'r')
+#             while True:
+#                 line=edge_file.readline()
+#                 if not line:
+#                     break
+#                 line_info=line.split('\n')[0].split('\t')
+#                 citesser.add_edge(Edge(line_info[0],line_info[1]))
+#             edge_file.close()
+#     return citesser
+
+# content_path="G:/IIT/CS583 TA/project/cora/cora.content"
+# cites_path="G:/IIT/CS583 TA/project/cora/cora.cites"
+
+# content_path="G:/IIT/CS583 TA/project/citeseer/citeseer.content"
+# cites_path="G:/IIT/CS583 TA/project/citeseer/citeseer.cites"
+# citeseer=load_citeseer([content_path,cites_path])
+
+# print
