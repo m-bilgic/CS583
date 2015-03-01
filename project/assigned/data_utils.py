74877826e7a25eaf8a464b36f6c4edd76254c4d0
Mustafa Bilgic <mbilgic@iit.edu>
2015-03-01 16:17:19 -0600

    data_utils now has one method called load_linqs_data. It returns the graph and the domain labels.

--- project/assigned/data_utils.py
+++ project/assigned/data_utils.py
@@ -1,20 +1,9 @@
 from graph import DirectedGraph, Node, Edge
 
-def _load_linqs_graph(data_path):
+def load_linqs_data(content_file, cites_file):
     '''
     Create a DirectedGraph object and add Nodes and Edges
     This is specific to the data files provided at http://linqs.cs.umd.edu/projects/projects/lbc/index.html
+    Return two items 1. graph object, 2. the list of domain labels (e.g., ['AI', 'IR'])
     '''
     raise NotImplementedError('You need to implement this method')
-
-def load_cora(data_path):
-    '''
-    Load cora data set.
-    '''
-    return _load_linqs_graph(data_path)
-
-def load_citeseer(data_path):
-    '''
-    Load citeseer data set
-    '''
-    return _load_linqs_graph(data_path)
